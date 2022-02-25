71b9b6f3c877ec317a17187268da36ce9870338a
wilsonmike <michaelwilsonsj@hotmail.com>
2022-02-25 13:12:15 -0500

    added axios && fetch data from crypto api

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import { API } from 'aws-amplify'
 import './App.css';
 
 function App() {
+  // state to hold user input for limit and start
+  const [input, updateInput] = useState({ limit: 5, start: 0})
+
+  // state for coins
   const [coins, updateCoins] = useState([])
-  
+
+  // define function for users to update input values
+  function updateInputValues(type, value) {
+    updateInput({...input, [type]: value})
+  }
+
   // define function to all api
   async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins')
+    const { limit, start } = input
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
     updateCoins(data.coins)
   }
+
 
   // call fetchCoins when component loads
   useEffect(() => {
@@ -19,6 +30,10 @@ function App() {
 
   return (
     <div className="App">
+      <input onChange={e => updateInputValues('limit', e.target.value)} placeholder="limit" />
+      <input onChange={e => updateInputValues('start', e.target.value)} placeholder="start" />
+      <button onClick={fetchCoins}>fetch coins</button>
+
       {
         coins.map((coin, index) => (
           <div key={index}>
